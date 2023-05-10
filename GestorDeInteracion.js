a596e099ae38d2ab6516847703a4d8ed88fe466a
Peluca82 <santiago-af@live.com.ar>
2023-05-10 16:19:58 -0300

    Cambios

--- GestorDeInteracion.js
+++ GestorDeInteracion.js
@@ -15,7 +15,7 @@ class GestorDeInteracion {
     this.tiempoGrande = constrain(this.tiempoGrande, 0, 90);
     this.tiempoPeque = constrain(this.tiempoPeque, 0, 90);
     if (this.mouse.velocidad()>10){
-      let umbral = 40;
+      let umbral = 50;
       if(this.mouse.velocidad() > umbral){
         this.tiempoGrande+=10;
         this.tiempoPeque-=10;
@@ -32,4 +32,9 @@ class GestorDeInteracion {
       this.movPeque = true;
     }
   } 
+
+  reset() {
+    this.tiempoGrande = 1;
+    this.tiempoPeque = 1;
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 
 let img2;
-let cant = 1;
+let cant = 0;
 let miVelocidadYDireccion;
 let img1;
 let gestor;
+let cantColores = [];
+
+let celeste = 0;
+let azul = 0;
+let gris = 0;
+let amarillo = 0;
+let rosa = 0;
 
 function preload() {
   img2 = loadImage('data/rect.png');
@@ -20,37 +27,66 @@ function setup() {
 }
 
 function draw() {
+
   miVelocidadYDireccion.calcularTodo(mouseX, mouseY);
-
   let velocidad = miVelocidadYDireccion.velocidad();
-  let direccionX = miVelocidadYDireccion.direccionX();
-  let direccionY = miVelocidadYDireccion.direccionY();
-
   gestor.actualizar();
 
   if(cant <= 250){
-    let x = random(75,width-75);
+
+    let x = random(75,width-75); 
+    // CAMBIAR PARA NO SEA COMLETAMENTE ALEATORIO
     let y = random(75,height-75);
-    if((cant > 0 && cant <= 100) && (velocidad >= 30 && velocidad <= 50)){//-------------------celeste
+
+    if((mouseX > 0 && mouseX < 600 && mouseY > 266 && mouseY < 533) && celeste <= 100 && velocidad > 5 && velocidad < 15){//-------------------celeste
       tint(52,168,215); 
-      image(img2,x,y);
-      cant = cant + 1;
-    } else if(cant > 100 && cant <= 175 && (velocidad >= 30 && velocidad <= 50)){//-------------azul
+
+      if (frameCount%5 == 0){
+        image(img2,x,y);
+        cant = cant + 1;
+        celeste++;
+      }
+
+    } else if((mouseX > 0 && mouseX < 300 && mouseY > 0 && mouseY < 266) && azul <= 75 && velocidad > 20){//-------------azul
+      gestor.reset();
       tint(0,71,123); 
-      image(img2,x,y);
-      cant = cant + 1;
-    }else if(cant > 175 && cant <= 200 && (velocidad >= 30 && velocidad <= 50)){//--------------gris
+
+      if (frameCount%5 == 0){
+        image(img2,x,y);
+        cant = cant + 1;
+        azul++;
+      }
+
+    }else if((mouseX > 300 && mouseX < 600 && mouseY > 0 && mouseY < 266) && gris <= 25 && velocidad > 20){//--------------gris
+      gestor.reset();
       tint(143,169,186);
-      image(img2,x,y);
-      cant = cant + 1;
-    }else if(cant > 200 && cant <= 225 && (velocidad >= 30 && velocidad <= 50)){//--------------amarillo
+
+      if (frameCount%5 == 0){
+        image(img2,x,y);
+        cant = cant + 1;
+        gris++;
+      }
+
+    }else if((mouseX > 0 && mouseX < 300 && mouseY > 533 && mouseY < 800) && amarillo <= 25 && velocidad > 20){//--------------amarillo
+      gestor.reset();
       tint(252,233,104);
-      image(img2,x,y);
-      cant = cant + 1;
-    }else if(cant > 225 && cant <= 250 && (velocidad >= 30 && velocidad <= 50)){//--------------rosa
+
+      if (frameCount%5 == 0){
+        image(img2,x,y);
+        cant = cant + 1;
+        amarillo++;
+      }
+
+    }else if((mouseX > 300 && mouseX < 600 && mouseY > 533 && mouseY < 800) && rosa <= 25 && velocidad > 20){//--------------rosa
+      gestor.reset();
       tint(244,53,170);
-      image(img2,x,y);
-      cant = cant + 1;
+
+      if (frameCount%5 == 0){
+        image(img2,x,y);
+        cant = cant + 1;
+        rosa++;
+      }
+
     }
   }
 }
