d7689f7419f1c68a39c811d53dccc0c3b709394c
Peluca82 <santiago-af@live.com.ar>
2023-06-09 23:26:04 -0300

    TRAZOS

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ let rosa = 0;
 function preload() {
 
   //-------CARGA DE TRAZOS Y RECTANGULOS-------
-  for (let i = 0; i < 12; i++){
+  for (let i = 0; i < 32; i++){
     let nombre = "data/trazo"+nf( i , 2 )+".png";
     arreglo[i] = loadImage(nombre);
     rect.push (loadImage('data/rect.png'));
@@ -28,7 +28,7 @@ function preload() {
 function setup() {
 
   createCanvas(600, 800);
-  background(240);                 
+  background(255);                 
   imageMode(CENTER);
 
   //-------DIRECCION Y INTERACCION-------
@@ -36,7 +36,7 @@ function setup() {
   gestor = new GestorDeInteracion();
 
   //-------APLICACION DE MASCARA A LOS RECTANGULOS-------
-  for (let i = 0; i < 12; i++){
+  for (let i = 0; i < 32; i++){
     rect[i].mask (arreglo[i]);
   }
 
@@ -50,16 +50,17 @@ function draw() {
   gestor.actualizar();
   
   //-------APLICACION DE MASCARA A LOS RECTANGULOS-------
-  let trazoRandom = rect[int(random(rect.length))];
+  let trazoRandom = rect[int(random(rect.length))];            //ESTO ES PARA LA MASCARA
+  //let trazoRandom = arreglo[int(random(arreglo.length))];
 
   if(cant <= 250){
 
     // CAMBIAR PARA NO SEA COMLETAMENTE ALEATORIO
-    let x = random(75,width-75); 
-    let y = random(75,height-75);
+    let x = random(100,width-100); 
+    let y = random(200,height-200);
 
     //-------USAR COLOR CELESTE-------
-    if((mouseX > 0 && mouseX < 600 && mouseY > 266 && mouseY < 533) && celeste <= 100 && velocidad > 5 && velocidad > 5){//-------------------celeste
+    if((mouseX > 0 && mouseX < width && mouseY > 266 && mouseY < 533) && celeste <= 10 && velocidad > 5 && velocidad > 5){//-------------------celeste
       tint(52,168,215);
 
       if (frameCount%5 == 0){
@@ -69,7 +70,7 @@ function draw() {
       }
 
     //-------USAR COLOR AZUL-------
-    } else if((mouseX > 0 && mouseX < 300 && mouseY > 0 && mouseY < 266) && azul <= 75 && velocidad > 10){//-------------azul
+    } else if((mouseX > 0 && mouseX < 300 && mouseY > 0 && mouseY < 266) && azul <= 10 && velocidad > 10){//-------------azul
       gestor.reset();
       tint(0,71,123); 
 
@@ -80,7 +81,7 @@ function draw() {
       }
 
     //-------USAR COLOR GRIS-------
-    }else if((mouseX > 300 && mouseX < 600 && mouseY > 0 && mouseY < 266) && gris <= 25 && velocidad > 10){//--------------gris
+    }else if((mouseX > 300 && mouseX < 600 && mouseY > 0 && mouseY < 266) && gris <= 5 && velocidad > 10){//--------------gris
       gestor.reset();
       tint(143,169,186);
 
@@ -91,7 +92,7 @@ function draw() {
       }
 
     //-------USAR COLOR AMARILLO-------
-    }else if((mouseX > 0 && mouseX < 300 && mouseY > 533 && mouseY < 800) && amarillo <= 10 && velocidad > 10){//--------------amarillo
+    }else if((mouseX > 0 && mouseX < 300 && mouseY > 533 && mouseY < 800) && amarillo <= 3 && velocidad > 10){//--------------amarillo
       gestor.reset();
       tint(252,233,104);
 
@@ -102,7 +103,7 @@ function draw() {
       }
 
     //-------USAR COLOR ROSA-------
-    }else if((mouseX > 300 && mouseX < 600 && mouseY > 533 && mouseY < 800) && rosa <= 10 && velocidad > 10){//--------------rosa
+    }else if((mouseX > 300 && mouseX < 600 && mouseY > 533 && mouseY < 800) && rosa <= 2 && velocidad > 10){//--------------rosa
       gestor.reset();
       tint(244,53,170);
 
